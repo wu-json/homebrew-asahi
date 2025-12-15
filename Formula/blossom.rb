ef94b5fe17858fbdd37aed35fbf2d2d1b0decdc4
wu-json <jason.wu2153@gmail.com>
2025-12-14 22:27:45 -0800

    feat: update formulas

---
+++ Formula/blossom.rb
@@ -0,0 +1,36 @@
+class Blossom < Formula
+  desc "AI conversational language learning app for Japanese, Chinese, and Korean"
+  homepage "https://github.com/wu-json/blossom"
+  version "0.0.2"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/wu-json/blossom/releases/download/v#{version}/blossom_#{version}_darwin_arm64.tar.gz"
+      sha256 "6414c476f3330121b1d6b4abf1af2b242d8b8af352a2e1fca85af0620f16739c"
+    end
+    on_intel do
+      url "https://github.com/wu-json/blossom/releases/download/v#{version}/blossom_#{version}_darwin_amd64.tar.gz"
+      sha256 "7f03c10aaa49a68a0a6079a8d75faad9fbf1f5bae7944a1183fb5ff108171696"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/wu-json/blossom/releases/download/v#{version}/blossom_#{version}_linux_arm64.tar.gz"
+      sha256 "827101faa7d77ce498485482c53ed66ca298e68933ba8c6841ab79895fadb8cd"
+    end
+    on_intel do
+      url "https://github.com/wu-json/blossom/releases/download/v#{version}/blossom_#{version}_linux_amd64.tar.gz"
+      sha256 "f0cb9e8feab62e1ba72f6b802d0927fa357e3cbc18f1822f0eda89ddd47562a1"
+    end
+  end
+
+  def install
+    bin.install "blossom"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/blossom --version")
+  end
+end
--- Formula/curse.rb
+++ Formula/curse.rb
@@ -1,28 +1,28 @@
 class Curse < Formula
   desc "Dead simple Terminal UI for running processes"
   homepage "https://github.com/wu-json/curse"
-  version "0.0.20"
+  version "0.0.21"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/wu-json/curse/releases/download/v#{version}/curse_#{version}_darwin_arm64.tar.gz"
-      sha256 "49b1b003b481543444de2831a127a5f93899273501d08c768c458a818ee909a5"
+      sha256 "093a741a5da7068f0531cbabff2337a304bfeb0725fcf6781989915f9e2665e1"
     end
     on_intel do
       url "https://github.com/wu-json/curse/releases/download/v#{version}/curse_#{version}_darwin_amd64.tar.gz"
-      sha256 "50537bd829c055ef6171aa89fbb5e086e0832e62e05785322f20a2d66c7fe3a7"
+      sha256 "8e1e6ff469d2294fa971d657577d6f32f3f11a857af10113a698e100d58e1458"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/wu-json/curse/releases/download/v#{version}/curse_#{version}_linux_arm64.tar.gz"
-      sha256 "914340d033ac4abfd2846458dc333c2de2d4e27593ab388b01e9be0b0771d747"
+      sha256 "3dbe131e228a989e109f95e47b656005f2e49b637d4d7ffbf8e53e12d5841d95"
     end
     on_intel do
       url "https://github.com/wu-json/curse/releases/download/v#{version}/curse_#{version}_linux_amd64.tar.gz"
-      sha256 "a46a5552169b4dd1c0faef471a10572644f861d5f68a0a7b47a409e20681bf6a"
+      sha256 "906522fe19676d8d906fe9f202e09eee74e2d63939d09f8a2328860239f83fb9"
     end
   end
 
