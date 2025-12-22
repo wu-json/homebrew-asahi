ecc05bb6720d69f69839accaf1fae3c48afe8c93
wu-json <jason.wu2153@gmail.com>
2025-12-22 08:54:20 -0800

    feat: update curse

--- Formula/curse.rb
+++ Formula/curse.rb
@@ -1,28 +1,28 @@
 class Curse < Formula
   desc "Dead simple Terminal UI for running processes"
   homepage "https://github.com/wu-json/curse"
-  version "0.0.21"
+  version "0.0.22"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/wu-json/curse/releases/download/v#{version}/curse_#{version}_darwin_arm64.tar.gz"
-      sha256 "093a741a5da7068f0531cbabff2337a304bfeb0725fcf6781989915f9e2665e1"
+      sha256 "cc52c6261f92f9dfda3b41ad5e9bb43ac19a3fe31f6d98868d1e470148560c9b"
     end
     on_intel do
       url "https://github.com/wu-json/curse/releases/download/v#{version}/curse_#{version}_darwin_amd64.tar.gz"
-      sha256 "8e1e6ff469d2294fa971d657577d6f32f3f11a857af10113a698e100d58e1458"
+      sha256 "76449e7d31795149f82cd056b2639946de5b14a5a7a998fab3024bdc2299a0f5"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/wu-json/curse/releases/download/v#{version}/curse_#{version}_linux_arm64.tar.gz"
-      sha256 "3dbe131e228a989e109f95e47b656005f2e49b637d4d7ffbf8e53e12d5841d95"
+      sha256 "f1719ac54378a365a98f41564c82200561aa7e484728eb452bc38718d7c29b01"
     end
     on_intel do
       url "https://github.com/wu-json/curse/releases/download/v#{version}/curse_#{version}_linux_amd64.tar.gz"
-      sha256 "906522fe19676d8d906fe9f202e09eee74e2d63939d09f8a2328860239f83fb9"
+      sha256 "e12c05ce907bf69f2b1f11f5effe2fc8ae51bff8bc094afd0c97855982f68ba2"
     end
   end
 
