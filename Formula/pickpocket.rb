cdf8158677be1d17a8b3aaa1dbc89fdb53e0ad32
wu-json <jason.wu2153@gmail.com>
2026-02-22 19:10:03 -0800

    feat: update pickpocket

--- Formula/pickpocket.rb
+++ Formula/pickpocket.rb
@@ -1,28 +1,28 @@
 class Pickpocket < Formula
   desc "Vendor git repos as local LLM context for your coding agents"
   homepage "https://github.com/wu-json/pickpocket"
-  version "0.0.2"
+  version "0.0.3"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/wu-json/pickpocket/releases/download/v#{version}/pickpocket_#{version}_darwin_arm64.tar.gz"
-      sha256 "ab0271ebbff46fee7591e8d07451f3eab40d9f1e3a19e42725c4321acda316d0"
+      sha256 "34f11531a7d465d8d4b4ff34ed53c0f66386ab8890f1fd6f336811488cc8810b"
     end
     on_intel do
       url "https://github.com/wu-json/pickpocket/releases/download/v#{version}/pickpocket_#{version}_darwin_amd64.tar.gz"
-      sha256 "d7d86d231cb679ce3a8763e1667747aba27acfa89432a7698ef55327f5681bb0"
+      sha256 "92ec913dc09a2f068297f14535ef7d44cbb2babf85ec2e258161b87720946e59"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/wu-json/pickpocket/releases/download/v#{version}/pickpocket_#{version}_linux_arm64.tar.gz"
-      sha256 "6c5bfde32f28d51ab2d35a136fc8252fca9b4dbd46b5ecedb3b0ec3d3b12d505"
+      sha256 "9bd7c25237e2d340636af917876e54bf290c8401709d35e01d39479f5e9ea97d"
     end
     on_intel do
       url "https://github.com/wu-json/pickpocket/releases/download/v#{version}/pickpocket_#{version}_linux_amd64.tar.gz"
-      sha256 "db1f4e3a514d2ed15e17def42a733d1941361326d67a89d308096b42dd8bec3f"
+      sha256 "181d973593a0d3cc676ac07397acea77faaa0a3d99799af93ee699bca0e6bc3e"
     end
   end
 
